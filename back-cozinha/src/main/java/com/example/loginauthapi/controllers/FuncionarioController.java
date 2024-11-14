34201451828e6898107780cfbe38f79ae9ed62dc
SamuelRicardos <samuelric4rdo@gmail.com>
2024-11-14 10:58:38 -0300

    Apenas pegar lógica do FuncionarioController

--- back-cozinha/src/main/java/com/example/loginauthapi/controllers/FuncionarioController.java
+++ back-cozinha/src/main/java/com/example/loginauthapi/controllers/FuncionarioController.java
@@ -1,8 +1,12 @@
 package com.example.loginauthapi.controllers;
 
+import java.util.Date;
 import java.util.List;
 
-
+import com.example.loginauthapi.dto.RegisterRequestDTO;
+import com.example.loginauthapi.model.Cargo;
+import com.example.loginauthapi.repositories.CargoRepository;
+import com.example.loginauthapi.repositories.FuncionarioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -26,22 +30,41 @@ import jakarta.validation.Valid;
 public class FuncionarioController {
     @Autowired
     private FuncionarioService funcionarioService;
+    @Autowired
+    private FuncionarioRepository funcionarioRepository;
+    @Autowired
+    private CargoRepository cargoRepository;
 
     @GetMapping("/")
     public List<Funcionario> List(){
         return funcionarioService.list();
     }
+
     @PostMapping("/")
-    public Funcionario create(@RequestBody @Valid Funcionario funcionario){
-        return funcionarioService.create(funcionario);
-    } 
+    public Funcionario create(@RequestBody @Valid RegisterRequestDTO body){
+        Cargo cargo = findByNome(body.nome_cargo());
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setRg(body.rg());
+        funcionario.setSalario(body.salario());
+        funcionario.setCargo(cargo);
+        funcionario.setNome(body.nome());
+        funcionario.setData_ade(new Date());
+        return funcionarioRepository.save(funcionario);
+    }
+
     @PutMapping("/")
     public Funcionario update(@RequestBody @Valid Funcionario funcionario){
         return funcionarioService.update(funcionario);
     }
+
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable("id") Long id){
         funcionarioService.delete(id);
         return ResponseEntity.ok().build();
+    }
+
+    private Cargo findByNome(String nome){
+        return cargoRepository.findByNome(nome).orElseThrow(()-> new RuntimeException("nome do cargo não encontrado"));
     }
 }
